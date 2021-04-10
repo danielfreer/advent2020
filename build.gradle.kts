de52ea2fbfb145ce0c274eadb02264e67dda26a0
Daniel Freer <danielfreer@gmail.com>
2021-04-10 00:05:23 -0400

    Configure project to run as application
    Resolves #4

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     kotlin("jvm") version "1.4.32"
+    application
 }
 
 group = "dev.danielfreer"
@@ -9,6 +10,10 @@ version = "0.1-SNAPSHOT"
 
 repositories {
     mavenCentral()
+}
+
+application {
+    mainClass.set("MainKt")
 }
 
 tasks.withType<KotlinCompile> {
